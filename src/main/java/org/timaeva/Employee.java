256fcf262895dc22cfaa9213c9c2854940127944
Alina1604 <alina.timayeva97@mail.ru>
2024-09-15 17:04:38 +0300

    Тимаева Алина Айратовна. Уровень: Новичок

---
+++ src/main/java/org/timaeva/Employee.java
@@ -0,0 +1,9 @@
+package org.timaeva;
+
+public class Employee {
+    public Long Id;
+    public String Email;
+    public String Phone;
+    public String Address;
+    public BankAccount BankAccount;
+}
---
+++ src/main/java/org/timaeva/Individual.java
@@ -0,0 +1,8 @@
+package org.timaeva;
+
+public class Individual extends Employee {
+    public String FirstName;
+    public String LastName;
+    public boolean HasChildren;
+    public int Age;
+}
